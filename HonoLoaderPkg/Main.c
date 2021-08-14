489c1dbd407f409bf31b25116baf71c1e2c89e95
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-08-15 05:37:40 +0900

    finish day03c

--- HonoLoaderPkg/Main.c
+++ HonoLoaderPkg/Main.c
@@ -238,9 +238,10 @@ EFI_STATUS EFIAPI UefiMain (
 
   UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
 
-  typedef void EntryPointType(void);
+  // FrameBuffer を Kernel に引き渡して起動する
+  typedef void EntryPointType(UINT64, UINT64);
   EntryPointType* entry_point = (EntryPointType*)entry_addr;
-  entry_point();
+  entry_point(gop->Mode->FrameBufferBase, gop->Mode->FrameBufferSize);
 
   Print(L"All Done\n");
 
