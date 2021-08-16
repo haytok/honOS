bd9d78c0ec4f58915f1c71fb13c35628130272a6
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-08-16 23:56:08 +0900

    finish day04d for creating logic of load kernel.elf

--- HonoLoaderPkg/Main.c
+++ HonoLoaderPkg/Main.c
@@ -3,12 +3,14 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/PrintLib.h>
 #include <Library/MemoryAllocationLib.h>
+#include <Library/BaseMemoryLib.h>
 #include <Protocol/LoadedImage.h>
 #include <Protocol/SimpleFileSystem.h>
 #include <Protocol/DiskIo2.h>
 #include <Protocol/BlockIo.h>
 #include <Guid/FileInfo.h>
 #include "frame_buffer_config.hpp"
+#include "elf.hpp"
 
 struct MemoryMap {
   UINTN buffer_size; // IN OUT
@@ -158,6 +160,33 @@ void Halt(void) {
   while (1) __asm__("hlt");
 }
 
+void CalcLoadAddressRange(Elf64_Ehdr* ehdr, UINT64* first, UINT64* last) {
+  Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+  *first = MAX_UINT64;
+  *last = 0;
+  for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
+    if (phdr[i].p_type != PT_LOAD) continue;
+    *first = MIN(*first, phdr[i].p_vaddr);
+    *last = MAX(*last, phdr[i].p_vaddr + phdr[i].p_memsz);
+  }
+}
+
+void CopyLoadSegments(Elf64_Ehdr* ehdr) {
+  Elf64_Phdr* phdr = (Elf64_Phdr*)((UINT64)ehdr + ehdr->e_phoff);
+  for (Elf64_Half i = 0; i < ehdr->e_phnum; ++i) {
+    if (phdr[i].p_type != PT_LOAD) continue;
+
+    // LODA のアドレス
+    UINT64 segm_in_file = (UINT64)ehdr + phdr[i].p_offset;
+    // 仮想アドレスに LODA をコピーする
+    CopyMem((VOID*)phdr[i].p_vaddr, (VOID*)segm_in_file, phdr[i].p_filesz);
+
+    // 0 埋め
+    UINTN remain_bytes = phdr[i].p_memsz - phdr[i].p_filesz;
+    SetMem((VOID*)(phdr[i].p_vaddr + phdr[i].p_filesz), remain_bytes, 0);
+  }
+}
+
 // Entry Point
 EFI_STATUS EFIAPI UefiMain (
     EFI_HANDLE image_handle,
@@ -224,12 +253,11 @@ EFI_STATUS EFIAPI UefiMain (
     frame_buffer[i] = 255;
   }
 
-  // Kernel
+  // Kernel の読み出し
   EFI_FILE_PROTOCOL* kernel_file;
   status = root_dir->Open(
     root_dir, &kernel_file, L"\\kernel.elf",
-    EFI_FILE_MODE_READ, 0
-  );
+    EFI_FILE_MODE_READ, 0);
   if (EFI_ERROR(status)) {
     Print(L"Failed to open file '\\kernel.elf': %r\n", status);
     Halt();
@@ -239,8 +267,7 @@ EFI_STATUS EFIAPI UefiMain (
   UINT8 file_info_buffer[file_info_size];
   status = kernel_file->GetInfo(
     kernel_file, &gEfiFileInfoGuid,
-    &file_info_size, file_info_buffer
-  );
+    &file_info_size, file_info_buffer);
   if (EFI_ERROR(status)) {
     Print(L"Failed to get fiel information: %r\n", status);
     Halt();
@@ -249,24 +276,42 @@ EFI_STATUS EFIAPI UefiMain (
   EFI_FILE_INFO* file_info = (EFI_FILE_INFO*)file_info_buffer;
   UINTN kernel_file_size = file_info->FileSize;
 
-  EFI_PHYSICAL_ADDRESS kernel_base_addr = 0x100000;
-  status = gBS->AllocatePages(
-    AllocateAddress, EfiLoaderData,
-    (kernel_file_size + 0xfff) / 0x1000, &kernel_base_addr
-  );
+  // カーネルファイルを一時的に読み出すためのメモリ領域を確保する。
+  VOID* kernel_buffer;
+  status = gBS->AllocatePool(EfiLoaderData, kernel_file_size, &kernel_buffer);
   if (EFI_ERROR(status)) {
-    Print(L"Failed to allocate pages: %r\n", status);
+    Print(L"failed to allocate pool: %r\n", status);
     Halt();
   }
 
-  status = kernel_file->Read(
-    kernel_file, &kernel_file_size, (VOID*)kernel_base_addr
-  );
+  // 一時的に確保したメモリ領域にカーネルを読み出す。
+  status = kernel_file->Read(kernel_file, &kernel_file_size, kernel_buffer);
   if (EFI_ERROR(status)) {
     Print(L"error: %r\n", status);
     Halt();
   }
-  Print(L"Kernel: 0x%0lx (%lu bytes)\n", kernel_base_addr, kernel_file_size);
+
+  Elf64_Ehdr* kernel_ehdr = (Elf64_Ehdr*)kernel_buffer;
+  UINT64 kernel_first_addr, kernel_last_addr;
+  CalcLoadAddressRange(kernel_ehdr, &kernel_first_addr, &kernel_last_addr);
+
+  // 最終的にカーネルを読み出すメモリ領域を確保
+  UINTN num_pages = (kernel_last_addr - kernel_first_addr + 0xfff) / 0x1000;
+  status = gBS->AllocatePages(AllocateAddress, EfiLoaderData,
+                              num_pages, &kernel_first_addr);
+  if (EFI_ERROR(status)) {
+    Print(L"failed to allocate pages: %r\n", status);
+    Halt();
+  }
+
+  CopyLoadSegments(kernel_ehdr);
+  Print(L"Kernel: 0x%0lx - 0x%0lx\n", kernel_first_addr, kernel_last_addr);
+
+  status = gBS->FreePool(kernel_buffer);
+  if (EFI_ERROR(status)) {
+    Print(L"failed to free pool: %r\n", status);
+    Halt();
+  }
 
   // Stop BootServices
   status = gBS->ExitBootServices(image_handle, memmap.map_key);
@@ -283,7 +328,7 @@ EFI_STATUS EFIAPI UefiMain (
     }
   }
 
-  UINT64 entry_addr = *(UINT64*)(kernel_base_addr + 24);
+  UINT64 entry_addr = *(UINT64*)(kernel_first_addr + 24);
 
   // FrameBuffer の Config オブジェクトを作成する
   struct FrameBufferConfig config = {
