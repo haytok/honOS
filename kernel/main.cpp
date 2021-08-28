cfa5161312c2a6854b2d1e6d923495145fa0be6e
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-08-28 18:08:09 +0900

    finish day09b for counting time

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -26,6 +26,7 @@
 #include "memory_manager.hpp"
 #include "window.hpp"
 #include "layer.hpp"
+#include "timer.hpp"
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
@@ -55,7 +56,11 @@ unsigned int mouse_layer_id;
 
 void MouseObserver(int8_t displacement_x, int8_t displacement_y) {
   layer_manager->MoveRelative(mouse_layer_id, {displacement_x, displacement_y});
+  StartLAPICTimer();
   layer_manager->Draw();
+  auto elapsed = LAPICTimerElapsed();
+  StopLAPICTimer();
+  printk("MouseObserver: elapsed = %u\n", elapsed);
 }
 
 void SwitchEhci2Xhci(const pci::Device& xhc_dev) {
@@ -121,6 +126,8 @@ extern "C" void KernelMainNewStack(
   console->SetWriter(pixel_writer);
   printk("Welcome to HonOS!!!\n");
   SetLogLevel(kWarn);
+
+  InitializeLAPICTimer();
 
   // UEFI から OS にメモリの情報を持っていき、メモリのセグメンテーションの設定
   SetupSegments();
