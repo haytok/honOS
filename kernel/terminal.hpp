1b5d45398ef8c5f38d4f43458ca6f7cad4ce9aaa
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-10-02 06:14:08 +0900

    finish day24e for creating logic of running applications in the background

--- kernel/terminal.hpp
+++ kernel/terminal.hpp
@@ -13,7 +13,7 @@ class Terminal {
   static const int kRows = 15, kColumns = 60;
   static const int kLineMax = 128;
 
-  Terminal(uint64_t task_id);
+  Terminal(uint64_t task_id, bool show_window);
   unsigned int LayerID() const { return layer_id_; }
   Rectangle<int> BlinkCursor();
   Rectangle<int> InputKey(uint8_t modifier, uint8_t keycode, char ascii);
@@ -41,6 +41,8 @@ class Terminal {
   std::deque<std::array<char, kLineMax>> cmd_history_{};
   int cmd_history_index_{-1}; // -1 は履歴を遡っていない状態を表す。
   Rectangle<int> HistoryUpDown(int direction);
+
+  bool show_window_;
 };
 
 extern std::map<uint64_t, Terminal*>* terminals;
